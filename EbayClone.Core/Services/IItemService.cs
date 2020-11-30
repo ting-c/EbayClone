733358c1d65e30fded445f5035a3abbfc62ee515
ting-c <ting.cheong31@gmail.com>
2020-11-30 20:04:19 +0000

    add IMusicService

---
+++ EbayClone.Core/Services/IItemService.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EbayClone.Core.Models;
+
+namespace EbayClone.Core.Services
+{
+    public interface IItemService
+    {
+        Task<IEnumerable<Item>> GetAllWithUser();
+        Task<Item> GetItemById(int id);
+        Task<IEnumerable<Item>> GetItemsByUserId(int userId);
+        Task<Item> CreateItem(Item newItem);
+        Task UpdateItem(Item itemToBeUpdated, Item item);
+        Task DeleteItem(Item item);
+    }
+}

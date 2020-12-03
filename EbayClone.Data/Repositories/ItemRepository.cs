10d4ff13208069fb9a6bccf3862ef8ac51949c59
ting-c <ting.cheong31@gmail.com>
2020-12-03 21:51:32 +0000

    add ItemRepository

---
+++ EbayClone.Data/Repositories/ItemRepository.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EbayClone.Core.Models;
+using EbayClone.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EbayClone.Data.Repositories
+{
+    public class ItemRepository : Repository<Item>, IItemRepository
+    {
+        public ItemRepository(EbayCloneDbContext context) : base(context)
+        { }
+
+        public async Task<IEnumerable<Item>> GetAllWithUserAsync()
+        {
+            return await EbayCloneDbContext.Items
+                .Include(i => i.Seller)
+                .ToListAsync();
+        }
+
+        public async Task<Item> GetWithUserByIdAsync(int itemId)
+        {
+            return await EbayCloneDbContext.Items
+				.Include(i => i.Seller)
+                .SingleOrDefaultAsync(i => i.Id == itemId);
+        }
+
+		public async Task<IEnumerable<Item>> GetAllWithUserbyUserIdAsync(int userId)
+		{
+			return await EbayCloneDbContext.Items
+                .Include(i => i.Seller)
+                .Where(i => i.SellerId == userId)
+                .ToListAsync();
+		}
+
+        private EbayCloneDbContext EbayCloneDbContext
+        {
+            get { return Context as EbayCloneDbContext; }
+        }
+    }
+}

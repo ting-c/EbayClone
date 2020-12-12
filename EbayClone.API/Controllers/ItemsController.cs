538d798308f1848e063e38545bcd4c7cc9ee9f0f
ting-c <ting.cheong31@gmail.com>
2020-12-12 09:43:32 +0000

    add GET method - GetAllItems in ItemsController

---
+++ EbayClone.API/Controllers/ItemsController.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EbayClone.Core.Models;
+using EbayClone.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EbayClone.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemService _itemService;
+
+        public ItemsController(IItemService itemService, IMapper mapper)
+        {
+            this._itemService = itemService;
+        }
+        
+        [HttpGet("")]
+        public async Task<ActionResult<IEnumerable<Item>>> GetAllItems()
+        {
+            var items = await _itemService.GetAllWithUser();
+            return Ok(items);
+        }
+			
+    }
+}

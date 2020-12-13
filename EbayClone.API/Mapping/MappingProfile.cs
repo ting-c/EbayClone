bfd5e1c6f9d547d0487108f249988b41389a8c17
ting-c <ting.cheong31@gmail.com>
2020-12-13 10:57:10 +0000

    add MappingProfiles for AutoMapper

---
+++ EbayClone.API/Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using EbayClone.API.Resources;
+using EbayClone.Core.Models;
+
+namespace EbayClone.API.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {   
+            // Domain to Resource
+            CreateMap<Item, ItemResource>();
+            CreateMap<User, UserResource>();
+
+            //Resource to Domain
+            CreateMap<ItemResource, Item>();
+            CreateMap<UserResource, User>();
+        }
+    }
+}

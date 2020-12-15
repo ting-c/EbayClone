6d553d6b4048d7e7cafe484c6881e1b4586f7014
ting-c <ting.cheong31@gmail.com>
2020-12-15 20:41:15 +0000

    add POST method for creating an user

--- EbayClone.API/Controllers/UsersController.cs
+++ EbayClone.API/Controllers/UsersController.cs
@@ -2,8 +2,10 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using EbayClone.API.Resources;
+using EbayClone.API.Validators;
 using EbayClone.Core.Models;
 using EbayClone.Core.Services;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EbayClone.API.Controllers
@@ -34,11 +36,29 @@ namespace EbayClone.API.Controllers
         public async Task<ActionResult<UserResource>> GetUserById(int id)
         {
             var user = await _userService.GetUserById(id);
-
             var userResource = _mapper.Map<User, UserResource>(user);
 
             return Ok(userResource);
         }
 
+        [HttpPost("")]
+        public async Task<ActionResult<UserResource>> CreateUser([FromBody] SaveUserResource saveUserResource)
+        {
+            var validator = new SaveUserResourceValidator();
+            ValidationResult results = await validator.ValidateAsync(saveUserResource);
+
+            if (results.IsValid)
+                return BadRequest(results.Errors);
+
+            User userToCreate = _mapper.Map<SaveUserResource, User>(saveUserResource);
+
+            var newUser = await _userService.CreateUser(userToCreate); 
+
+            var user = await _userService.GetUserById(newUser.Id);
+
+            UserResource userResource = _mapper.Map<User, UserResource>(user);
+
+            return Ok(userResource);
+        }
     }
 }

59a9f407b881892f381f2c6160a41a09d6d19893
ting-c <ting.cheong31@gmail.com>
2020-12-24 17:00:24 +0000

    refactor item list in ItemServiceTests

--- backend/Tests/Services/ItemServiceTests.cs
+++ backend/Tests/Services/ItemServiceTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using EbayClone.Core;
 using EbayClone.Core.Models;
@@ -11,68 +12,13 @@ namespace Tests.Services
     public class ItemServiceTests
     {
 		private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly List<Item> _items;
 
         public ItemServiceTests()
         {
             this._mockUnitOfWork = new Mock<IUnitOfWork>();
-        }
-
-        [Fact]
-        public async Task GetAllWithUser_ReturnAllItems()
-        {
-            // Arrange
-            var items = new List<Item>()
+            this._items = new List<Item>()
             {
-                new Item()
-                {
-                    Title = "Title 1",
-                    SellerId = 1
-                },
-                new Item()
-                {
-                    Title = "Title 2",
-                    SellerId = 2
-                }
-            };
-            _mockUnitOfWork.Setup(u => u.Items.GetAllWithUserAsync())
-                .ReturnsAsync(items);
-            var itemService = new ItemService(_mockUnitOfWork.Object);
-
-            // Act
-            var result = await itemService.GetAllWithUser();
-
-            // Assert
-            Assert.Equal<IEnumerable<Item>>(items, result);
-        }
-
-        [Fact]
-        public async Task GetItemById_ReturnItem()
-        {
-            // Arrange
-            var item = new Item()
-            {   
-                Id = 1,
-                Title = "Title 1",
-                SellerId = 1
-            };
-            _mockUnitOfWork.Setup(u => u.Items.GetWithUserByIdAsync(item.Id))
-                .ReturnsAsync(item);
-            var itemService = new ItemService(_mockUnitOfWork.Object);
-
-            // Act
-            var result = await itemService.GetItemById(item.Id);
-
-            // Assert
-            Assert.Equal<Item>(item, result);
-        }
-
-        [Fact]
-        public async Task GetItemsByUserId_ReturnItems()
-        {
-            // Arrange
-            var sellerId = 1;
-			var items = new List<Item>()
-			{
 				new Item()
 				{
 					Title = "Title 1",
@@ -81,18 +27,62 @@ namespace Tests.Services
 				new Item()
 				{
 					Title = "Title 2",
-					SellerId = 1
+					SellerId = 2
 				}
-			};
-            _mockUnitOfWork.Setup(u => u.Items.GetAllWithUserbyUserIdAsync(sellerId))
-                .ReturnsAsync(items);
+            };
+        }
+
+        [Fact]
+        public async Task GetAllWithUser_ReturnAllItems()
+        {
+            // Arrange
+            _mockUnitOfWork.Setup(u => u.Items.GetAllWithUserAsync())
+                .ReturnsAsync(_items);
             var itemService = new ItemService(_mockUnitOfWork.Object);
 
             // Act
-            var result = await itemService.GetItemsByUserId(sellerId);
+            var result = await itemService.GetAllWithUser();
 
             // Assert
-            Assert.Equal<IEnumerable<Item>>(items, result);
+            Assert.Equal<IEnumerable<Item>>(_items, result);
+        }
+
+        [Fact]
+        public async Task GetItemById_ReturnItem()
+        {
+            // Arrange
+            var testItemId = 1;
+            var expectedItem = _items.FirstOrDefault(
+                i => i.Id == testItemId
+            );
+            _mockUnitOfWork.Setup(u => u.Items.GetWithUserByIdAsync(testItemId))
+                .ReturnsAsync(expectedItem);
+            var itemService = new ItemService(_mockUnitOfWork.Object);
+
+            // Act
+            var result = await itemService.GetItemById(testItemId);
+
+            // Assert
+            Assert.Equal<Item>(expectedItem, result);
+        }
+
+        [Fact]
+        public async Task GetItemsByUserId_ReturnItems()
+        {
+            // Arrange
+            var testSellerId = 1;
+            var expectedItems = _items.FindAll(
+                i => i.SellerId == testSellerId
+            );
+            _mockUnitOfWork.Setup(u => u.Items.GetAllWithUserbyUserIdAsync(testSellerId))
+                .ReturnsAsync(expectedItems);
+            var itemService = new ItemService(_mockUnitOfWork.Object);
+
+            // Act
+            var result = await itemService.GetItemsByUserId(testSellerId);
+
+            // Assert
+            Assert.Equal<IEnumerable<Item>>(expectedItems, result);
         }
     }
 }

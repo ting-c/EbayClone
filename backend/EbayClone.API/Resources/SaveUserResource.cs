96531d8eb5c6fc38a67ac7f7d37c3bb258edda84
ting-c <ting.cheong31@gmail.com>
2020-12-27 12:53:24 +0000

    update UserResource and SaveUserResource

--- backend/EbayClone.API/Resources/SaveUserResource.cs
+++ backend/EbayClone.API/Resources/SaveUserResource.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using EbayClone.Core.Models;
+
 namespace EbayClone.API.Resources
 {
     public class SaveUserResource : UserSignUpResource
     {
 		public string Address { get; set; }
 		public string PhoneNumber { get; set; }
+		public ICollection<Item> SellingItems { get; set; }
     }
 }
--- backend/EbayClone.API/Resources/UserResource.cs
+++ backend/EbayClone.API/Resources/UserResource.cs
@@ -1,8 +1,29 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using EbayClone.Core.Models;
+
 namespace EbayClone.API.Resources
 {
     public class UserResource
     {
+		public UserResource()
+		{
+			SellingItems = new Collection<Item>();
+		}
         public int Id { get; set; }
-        public string Name { get; set; }
+		public string Name
+		{
+			get
+			{
+				return this.FirstName + " " + LastName;
+			}
+		}
+		public string Email { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string UserName { get; set; }
+		public string Address { get; set; }
+		public string PhoneNumber { get; set; }
+		public ICollection<Item> SellingItems { get; set; }
     }
 }
